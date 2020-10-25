8990a553a05728220582659233e0962de67dc47c
Alp <49457057+alpkeskin@users.noreply.github.com>
2020-10-25 22:48:19 +0300

    Update mosint.py

--- mosint.py
+++ mosint.py
@@ -1,10 +1,9 @@
 #https://github.com/alpkeskin
-import re
 import requests
+import re
 from bs4 import BeautifulSoup
 import json
 import os
-from socialscan.util import Platforms, sync_execute_queries
 from prettytable import PrettyTable
 
 class bcolors:
@@ -25,7 +24,7 @@ ___  ________ _____ _____ _   _ _____
 | |  | \ \_/ /\__/ /_| |_| |\  | | |  
 \_|  |_/\___/\____/ \___/\_| \_/ \_/ 
 {bcolors.ENDC}
-v{bcolors.BOLD}1.2{bcolors.ENDC}
+v{bcolors.BOLD}1.3{bcolors.ENDC}
 github.com/{bcolors.BOLD}alpkeskin{bcolors.ENDC}
 ''')
 
@@ -66,29 +65,31 @@ def pwnconnect(url='https://scylla.sh/' , timeout=20):
     except requests.ConnectionError:
         print(f"{bcolors.FAIL}[-] No leaked database available.{bcolors.ENDC}")
     return False
-
 configfile = open('config.json', "r")
 conf = json.loads(configfile.read())
 for i in conf:
     checkconnect = (i['Check Connections'])
-    setapi = (i['API Key'])
+    verifyApi = (i['verify-email.org API Key'])
     socialscan = (i['Social Scan'])
     leakeddb = (i['Leaked DB'])
+    hunterApi = (i['shodan.io API Key'])
+    dbdata = (i['Related Phone Numbers'])
     pastebindumps = (i['Pastebin Dumps'])
     googlesearch = (i['Google Search'])
     dns = (i['DNS Lookup'])
+
 if (checkconnect == "True" or checkconnect == "T" or checkconnect == "true"):
     connection()
     verifyconnect()
     pwnconnect()
 
-verifyurl = "https://app.verify-email.org/api/v1/"+setapi+"/verify/"
-creditsurl = "https://app.verify-email.org/api/v1/"+setapi+"/credits"
+verifyurl = "https://app.verify-email.org/api/v1/"+verifyApi+"/verify/"
+creditsurl = "https://app.verify-email.org/api/v1/"+verifyApi+"/credits"
 leakedpassurl = "https://scylla.sh/search?q=email:"
 psbdmpurl = "https://psbdmp.ws/api/search/"
 searchurlP="https://s.sudonull.com/?q=site%3Apastebin.com+intext%3A%22"
 searchurlT="https://s.sudonull.com/?q=site%3Athrowbin.io+intext%3A%22"
-if (setapi != ""):
+if (verifyApi != ""):
     print('API Key : '+'\x1b[6;30;42m' + ' OK! ' + '\x1b[0m')
     response = requests.get(creditsurl)
     html = response.content
@@ -102,18 +103,19 @@ while True:
     if (mail == "q"):
         print("Thank you for using "+f"{bcolors.BOLD}MOSINT{bcolors.ENDC}.")
         break
-    elif (setapi != ""): 
-    	response1 = requests.get(verifyurl+str(mail))
-    	html1 = response1.content
-    	soup1=BeautifulSoup(html1,"html.parser")
-    	strsoup1 = str(soup1)
-    	data1 = json.loads(strsoup1)
-    	print(f"{bcolors.HEADER}[#]{bcolors.ENDC}" + " Verification result : "+str(data1['status_description']))
-    	print("")
-    	print("------------------------")
-    	print("")
+    if (verifyApi != ""): 
+        response1 = requests.get(verifyurl+str(mail))
+        html1 = response1.content
+        soup1=BeautifulSoup(html1,"html.parser")
+        strsoup1 = str(soup1)
+        data1 = json.loads(strsoup1)
+        print(f"{bcolors.HEADER}[#]{bcolors.ENDC}" + " Verification result : "+str(data1['status_description']))
+        print("")
+        print("------------------------")
+        print("")
     try:
         if (socialscan == "True" or socialscan == "T" or socialscan == "true"):
+            from socialscan.util import Platforms, sync_execute_queries
             queries = [mail]
             platforms = [Platforms.GITHUB, Platforms.TWITTER, Platforms.INSTAGRAM, Platforms.PINTEREST, Platforms.SPOTIFY]
             results = sync_execute_queries(queries, platforms)
@@ -136,13 +138,53 @@ while True:
             table = PrettyTable(["Domain","Email",f"{bcolors.FAIL}Password{bcolors.ENDC}"])
             for s in range(len(lp)):
                 table.add_row([lp[s]["fields"]["domain"],lp[s]["fields"]["email"],lp[s]["fields"]["password"]])
-            print(table)    
+            print(table)
             print("")    
             print("------------------------")  
             print("")
     except:
         print("DB Connection Error!")
         print("")
+
+    try:
+        dmnlist = ["gmail.com","outlook.com","hotmail.com","yahoo.com","hotmail.co.uk","icloud.com"]
+        at = "@"
+        domain = (mail[mail.index(at) + len(at):])
+        if (hunterApi != "" and domain in dmnlist):
+            print(f"{bcolors.FAIL}Unacceptable domain : {bcolors.ENDC}"+domain)
+            print("")    
+            print("------------------------")  
+            print("")
+        elif (hunterApi != ""):
+            print(f"{bcolors.BOLD}Related emails:{bcolors.ENDC}")
+            u = "https://api.hunter.io/v2/domain-search?domain="+domain+"&api_key="+hunterApi
+            response = requests.get(u)
+            html = response.content
+            lp = json.loads(html)
+            for i in range(0,99):
+                print(lp['data']['emails'][i]['value'])
+    except:
+        print("")    
+        print("------------------------")  
+        print("")
+
+    try:
+        if (dbdata == "True" or dbdata == "T" or dbdata == "true"):
+            dnsurl = ("https://domainbigdata.com/email/"+mail)
+            response = requests.get(dnsurl)
+            html = response.content
+            soup=BeautifulSoup(html,"html.parser")
+            rgx = str(soup)
+            urls = re.findall('<td colspan="2">\+.*.</td>', rgx)
+            print(f"{bcolors.BOLD}Related Phone Numbers:{bcolors.ENDC}")
+            for x in range(len(urls)): 
+                n = urls[x].replace("<td colspan="+'"'+'2'+'"'+'>', "")
+                print(n)
+            print("")    
+            print("------------------------")  
+            print("")            
+    except:
+        print("Phone Number Data Error!")
     try:
         if (pastebindumps == "True" or pastebindumps == "T" or pastebindumps == "true"):
             print(f"{bcolors.WARNING} -- Scanning Pastebin Dumps...{bcolors.ENDC}")
@@ -161,6 +203,7 @@ while True:
     try:
         if (googlesearch == "True" or googlesearch == "T" or googlesearch == "true"):
             print(f"{bcolors.WARNING} -- Google Searching... [Pastebin & Throwbin]{bcolors.ENDC}")
+            print(f"{bcolors.FAIL}!{bcolors.ENDC}"+"Google Search may not work properly.")
             print("")    
             x = mail.replace("@", "%40")
             u = (searchurlP+x+"%22")

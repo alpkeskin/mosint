ec5073c9692d0c99241920d9772880d41e25eadb
Alp <49457057+alpkeskin@users.noreply.github.com>
2020-11-23 13:55:08 +0300

    Update RelatedNumbers.py

--- modules/RelatedNumbers.py
+++ modules/RelatedNumbers.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests, re
-from modules.bcolors import bcolors
+from insides.bcolors import bcolors
 
 def RelatedNumbers(mail,_verbose=None):
 	if _verbose != None:
@@ -11,12 +11,8 @@ def RelatedNumbers(mail,_verbose=None):
 			soup=BeautifulSoup(html,"html.parser")
 			rgx = str(soup)
 			urls = re.findall('<td colspan="2">\+.*.</td>', rgx)
-			print(f"{bcolors.BOLD}Related Phone Numbers:{bcolors.ENDC}")
 			for x in range(len(urls)): 
 				n = urls[x].replace("<td colspan="+'"'+'2'+'"'+'>', "")
 				print(n)
-			print("")    
-			print("------------------------")  
-			print("")
 		except:
-			print("Phone Number Data Error!")
+			print(f"{bcolors.FAIL}Phone Number Data Error!{bcolors.ENDC}")

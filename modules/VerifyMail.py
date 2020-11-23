a53dbf7014345e507c1e0adbd18a0c269ab727cb
Alp <49457057+alpkeskin@users.noreply.github.com>
2020-11-23 13:54:20 +0300

    Update VerifyMail.py

--- modules/VerifyMail.py
+++ modules/VerifyMail.py
@@ -1,17 +1,15 @@
 from bs4 import BeautifulSoup
 import json,requests
-from modules.bcolors import bcolors
+from insides.bcolors import bcolors
 
 
 def VerifyMail(verifyApi,mail,_verbose=None):
         if _verbose != None:
+                
         	verifyurl = "https://app.verify-email.org/api/v1/"+verifyApi+"/verify/"
         	response1 = requests.get(verifyurl+str(mail))
         	html1 = response1.content
         	soup1=BeautifulSoup(html1,"html.parser")
         	strsoup1 = str(soup1)
         	data1 = json.loads(strsoup1)
-        	print(f"{bcolors.HEADER}[#]{bcolors.ENDC}" + " Verification result : "+str(data1['status_description']))
-        	print("")
-        	print("------------------------")
-        	print("")
+        	print(f"[{bcolors.HEADER}#{bcolors.ENDC}]" + " Verification result : "+str(data1['status_description']))

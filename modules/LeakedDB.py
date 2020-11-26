39de63ab286366a57b1de4ba716653209f2010d9
Çalgan Aygün <calganaygun@hacettepe.edu.tr>
2020-11-26 23:17:03 +0300

    Update LeakedDB method
    Bugs fixed, code cleaned

--- modules/LeakedDB.py
+++ modules/LeakedDB.py
@@ -1,22 +1,18 @@
-from bs4 import BeautifulSoup
-import json, requests
+import requests
 from insides.bcolors import bcolors
 from prettytable import PrettyTable
 
 def LeakedDB(mail,_verbose=None):
 	if _verbose != None:
 		try:
-			headers = { 
-			"Accept": "application/json" 
-			}
-			leakedpassurl = "https://scylla.sh/search?q=email:"
-			u = (leakedpassurl+mail)
-			response = requests.get(u,timeout=5,headers=headers)
-			html = response.content
-			lp = json.loads(html)
+			res = requests.get(f"https://scylla.sh/search?q=email:{mail}&size=50&start=0", headers={'Accept': 'application/json'}).json()
 			table = PrettyTable(["Domain","Email",f"{bcolors.FAIL}Password{bcolors.ENDC}"])
-			for s in range(len(lp)):
-				table.add_row([lp[s]["fields"]["domain"],lp[s]["fields"]["email"],lp[s]["fields"]["password"]])
-			print(table)
-		except:
+			if len(res):
+				for s in res:
+					table.add_row([s["fields"]["domain"],s["fields"]["email"],s["fields"].get("password") or s["fields"].get("passhash") or "No Pass Data"])
+				print(table)
+			else:
+				print(f"{bcolors.FAIL}No leaked accounts found!{bcolors.ENDC}")
+		except Exception as e:
+			print(e)
 			print(f"{bcolors.FAIL}Leaked DB Connection Error!{bcolors.ENDC}")

d1893cb344250ad97747ff7043db4c6903e9286f
Çalgan Aygün <calganaygun@hacettepe.edu.tr>
2020-11-26 23:13:05 +0300

    Update Google Search
    Google search package integreted, 3rd partysearch website removed, code cleaned

--- modules/Googling.py
+++ modules/Googling.py
@@ -1,5 +1,4 @@
-from bs4 import BeautifulSoup
-import re, requests
+from googlesearch import search
 from insides.bcolors import bcolors
 
 
@@ -7,29 +6,12 @@ def Googling(mail,_verbose=None):
 	if _verbose != None:
 		try:
 			print(f"{bcolors.WARNING} -- Google Searching... [Pastebin & Throwbin]{bcolors.ENDC}")
-			print(f"{bcolors.FAIL}!{bcolors.ENDC}"+"Google Search may not work properly.")
-			print("")    
-			x = mail.replace("@", "%40")
-			searchurlP="https://s.sudonull.com/?q=site%3Apastebin.com+intext%3A%22"
-			u = (searchurlP+x+"%22")
-			response = requests.get(u)
-			html = response.content
-			soup=BeautifulSoup(html,"html.parser")
-			rgx = str(soup)
-			urls = re.findall('http[s]?://pastebin.com(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+<', rgx)
-			for x in range(len(urls)): 
-				p = urls[x].replace("<", "")
-				print(f"{bcolors.OKGREEN}|-- {bcolors.ENDC}"+p)
-			x = mail.replace("@", "%40")
-			searchurlT="https://s.sudonull.com/?q=site%3Athrowbin.io+intext%3A%22"
-			u = (searchurlT+x+"%22")
-			response = requests.get(u)
-			html = response.content
-			soup=BeautifulSoup(html,"html.parser")
-			rgx = str(soup)
-			urls = re.findall('http[s]?://throwbin.io(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+<', rgx)
-			for x in range(len(urls)): 
-				t = urls[x].replace("<", "")
-				print(f"{bcolors.OKGREEN}|-- {bcolors.ENDC}"+t)
+			print(f"{bcolors.FAIL}!{bcolors.ENDC}"+"Google Search may not work properly.\n")
+			urls = search(f'site:throwbin.io intext:"{mail}"') + search(f'site:pastebin.com intext:"{mail}"')
+			if len(urls):
+				for x in urls: 
+					print(f"{bcolors.OKGREEN}|-- {bcolors.ENDC}"+x)
+			else:
+				print(f"{bcolors.FAIL}No Google Search result found!{bcolors.ENDC}")
 		except:
 			print(f"{bcolors.FAIL}Google Search error!{bcolors.ENDC}")

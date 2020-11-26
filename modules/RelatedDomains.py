6f6a8088a9177ab10540c316d6209f74ef11d378
Çalgan Aygün <calganaygun@hacettepe.edu.tr>
2020-11-26 23:15:06 +0300

    Update RelatedDomains method
    Bugs fixed, code cleaned

--- modules/RelatedDomains.py
+++ modules/RelatedDomains.py
@@ -5,12 +5,13 @@ from insides.bcolors import bcolors
 def RelatedDomains(mail,_verbose=None):
 	if _verbose != None:
 		try:
-			u = "https://www.threatcrowd.org/searchApi/v2/email/report/?email="+mail
-			response = requests.get(u)
-			html = response.content
-			lp = json.loads(html)
-			print(f"{bcolors.BOLD}Related Domains:{bcolors.ENDC}")
-			for x in lp['domains']:
-				print(x)
+			res = requests.get(f"https://www.threatcrowd.org/searchApi/v2/email/report/?email={mail}").json()
+			domains = res.get('domains') or []
+			if len(domains):
+				print(f"{bcolors.BOLD}Related Domains:{bcolors.ENDC}")
+				for domain in domains:
+					print(domain)
+			else:
+				print(f"{bcolors.FAIL}No related domains found!{bcolors.ENDC}")
 		except:
-			print(f"{bcolors.FAIL}Domain not found!{bcolors.ENDC}")
+			print(f"{bcolors.FAIL}Threatcrowd Error!{bcolors.ENDC}")

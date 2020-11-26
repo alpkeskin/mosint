51bcf448c316ff16459d1a29c1d3f2a661cdd3c6
Çalgan Aygün <calganaygun@hacettepe.edu.tr>
2020-11-26 23:15:37 +0300

    Update RealtedNumbers method
    Code cleaned, bug fixed

--- modules/RelatedNumbers.py
+++ modules/RelatedNumbers.py
@@ -2,17 +2,17 @@ from bs4 import BeautifulSoup
 import requests, re
 from insides.bcolors import bcolors
 
-def RelatedNumbers(mail,_verbose=None):
+def RelatedNumbers(mail, _verbose=None):
 	if _verbose != None:
 		try:
-			dnsurl = ("https://domainbigdata.com/email/"+mail)
-			response = requests.get(dnsurl)
-			html = response.content
+			html = requests.get(f"https://domainbigdata.com/email/{ mail }").content
 			soup=BeautifulSoup(html,"html.parser")
 			rgx = str(soup)
-			urls = re.findall('<td colspan="2">\+.*.</td>', rgx)
-			for x in range(len(urls)): 
-				n = urls[x].replace("<td colspan="+'"'+'2'+'"'+'>', "")
+			phones = re.findall('<td colspan="2">\+.*.</td>', rgx)
+			for phone in phones:
+				number = urls[x].replace("<td colspan="+'"'+'2'+'"'+'>', "").replace("</td>","")
 				print(n)
+			if len(phones) == 0:
+				print(f"{bcolors.FAIL}No phone numbers found!{bcolors.ENDC}")
 		except:
 			print(f"{bcolors.FAIL}Phone Number Data Error!{bcolors.ENDC}")

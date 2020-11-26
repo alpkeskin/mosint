a2e4220640d19bc46faf57fd42a2ac09d8ce9dff
Çalgan Aygün <calganaygun@hacettepe.edu.tr>
2020-11-26 23:14:07 +0300

    Update VerifyMail method
    Code cleaned, bult-in mail verifier added

--- modules/VerifyMail.py
+++ modules/VerifyMail.py
@@ -1,15 +1,13 @@
-from bs4 import BeautifulSoup
-import json,requests
+import requests
+from verify_email import verify_email
 from insides.bcolors import bcolors
 
 
-def VerifyMail(verifyApi,mail,_verbose=None):
-        if _verbose != None:
-                
-        	verifyurl = "https://app.verify-email.org/api/v1/"+verifyApi+"/verify/"
-        	response1 = requests.get(verifyurl+str(mail))
-        	html1 = response1.content
-        	soup1=BeautifulSoup(html1,"html.parser")
-        	strsoup1 = str(soup1)
-        	data1 = json.loads(strsoup1)
-        	print(f"[{bcolors.HEADER}#{bcolors.ENDC}]" + " Verification result : "+str(data1['status_description']))
+def VerifyMail(verifyAPIKey, mail, _verbose=None):
+	if _verbose != None:
+		res = requests.get(f"https://app.verify-email.org/api/v1/{verifyAPIKey}/verify/{mail}").json()
+		print(f"[{bcolors.HEADER}#{bcolors.ENDC}] Verify-Email.org result : {res.get('status_description')}")
+		if verify_email(mail):
+			print(f"[{bcolors.HEADER}#{bcolors.ENDC}] Built-in Mail Verify result : {bcolors.OKGREEN}OK{bcolors.ENDC}")
+		else:
+			print(f"[{bcolors.HEADER}#{bcolors.ENDC}] Built-in Mail Verify result : {bcolors.FAIL}FAILED{bcolors.ENDC}")

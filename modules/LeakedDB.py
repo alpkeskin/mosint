5530d62fab23214a5b5a75ef3837af33f236cc05
한국정보보호교육센터 <43199261+kisec@users.noreply.github.com>
2021-03-02 14:14:05 +0900

    Modify LeakedDB.py
    When Using the default LeakedDB.py, an error occurs.
    
    `Leaked DB Connection Error!`
    
    Need to change the following contents in LeakedDB.py.
    
    Testing Environment
    
    Ubuntu 20.04.1 LTS
    Python 3.8.5
    pip 20.0.2

--- modules/LeakedDB.py
+++ modules/LeakedDB.py
@@ -5,7 +5,7 @@ from prettytable import PrettyTable
 def LeakedDB(mail,_verbose=None):
 	if _verbose != None:
 		try:
-			res = requests.get(f"https://www.scylldb.com/search?q=email:{mail}&size=50&start=0", headers={'Accept': 'application/json'}).json()
+			res = requests.get(f"https://www.scylladb.com/search_gcse/?q=email:{mail}&size=50&start=0", headers={'Accept': 'application/json'}).json()
 			table = PrettyTable(["Domain","Email",f"{bcolors.FAIL}Password{bcolors.ENDC}"])
 			if len(res):
 				for s in res:

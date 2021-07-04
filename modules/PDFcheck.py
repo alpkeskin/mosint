fb98eb8bec397a739e2df6b8cc1e5a8fe2ffdf43
Alp <49457057+alpkeskin@users.noreply.github.com>
2021-07-04 14:32:27 +0300

    Update PDFcheck.py

--- modules/PDFcheck.py
+++ modules/PDFcheck.py
@@ -1,9 +1,11 @@
 from googlesearch import search
+from datetime import datetime
 import requests,re,PyPDF2,tabula,os
 
 def PDFcheck(mail,_verbose=None):
 	domain = mail.split("@")[1]
 	term = "site:"+domain+" filetype:PDF intext:"+'"'+"email"+'"'
+	emails = ""
 	try:
 		data = search(term, num_results=5)
 		for i in data:
@@ -20,6 +22,10 @@ def PDFcheck(mail,_verbose=None):
 					if(findPDFs[0] is not None):
 						for pdfs in findPDFs:
 							print(pdfs)
+							emails = emails + "\n" + pdfs
+					with open((datetime.today().strftime('%Y-%m-%d-'+domain)+".txt"), "w") as f:
+						f.write(emails)
+					f.close()
 				except:
 					pass
 			pdfFileObj.close()
